244ff7d58afeff61b071f702efd550b793543eed
Mingliang Jiang <mijiang@google.com>
2025-03-17 13:30:06 -0700

    worked version

---
+++ test_abc/main.py
@@ -0,0 +1,3 @@
+from pmt_dummy import test
+if __name__ == "__main__":
+    print(test.abc())
--- tools/artifact_registry/cred_helper.sh
+++ tools/artifact_registry/cred_helper.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-# cred_helper.sh
-ARG=$1  # but we don't do anything with it as it's always "get"
-
-function log () {
-    echo "$@" >> "/tmp/artifact_registry_auth.log"
-}
-
-log "$ARG"
-log "Authenticating"
-
-TOKEN="$(gcloud auth print-access-token)"
-log "$TOKEN"
-
-echo {"headers": {"Authorization: Bearer $TOKEN"}}
-
-log "Auth Complete"
+# lol I like pretty-printed JSON. It's not a requirement though.
+echo '{'
+echo '  "headers": {'
+echo '    "Authorization": ['
+echo '      "Bearer '$(gcloud auth print-access-token)'"'
+echo '    ]'
+echo '  }'
+echo '}'
