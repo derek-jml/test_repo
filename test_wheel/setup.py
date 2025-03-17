2a3cf0abcf171ee6626083bf69733c80ffb8c4ff
Mingliang Jiang <mijiang@google.com>
2025-03-17 12:38:08 -0700

    test dummy

---
+++ test_wheel/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pmt_dummy",
+    version="2025031701",
+    packages=find_packages(),
+    install_requires=[
+    'numpy>=1.20,<1.24',
+    ]
+)
